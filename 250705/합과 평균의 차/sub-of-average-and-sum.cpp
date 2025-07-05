40c56bd90cb86ed536289f5d90d56b65eb27a4c4
SeokHoon Kim <qttttt@naver.com>
2025-07-05 23:43:17 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int sum = a+b+c;
+    cout << sum << '\n' << sum/3 << '\n' << sum/3*2;
+    return 0;
+}
