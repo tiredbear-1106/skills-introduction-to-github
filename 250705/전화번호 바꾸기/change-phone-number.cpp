16cc2f482035e2c535d1bd891f443c11271c3676
SeokHoon Kim <qttttt@naver.com>
2025-07-05 17:39:07 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, x, y;
+    cin >> a;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+    cout << "010-" << y << '-' << x;
+    return 0;
+}
