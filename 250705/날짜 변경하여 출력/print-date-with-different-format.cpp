97b231fa1a1e45bbb1415a39aef19d870e36b2d4
SeokHoon Kim <qttttt@naver.com>
2025-07-05 17:19:38 +0900

    Title: 날짜 변경하여 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    cin >> y;
+    cin.get();
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cout << m << '-' << d << '-' << y;
+    return 0;
+}
