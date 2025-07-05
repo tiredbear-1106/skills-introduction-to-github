9bca3dae1acea3020e9c810a657b9030f93f0fd3
SeokHoon Kim <qttttt@naver.com>
2025-07-05 23:58:44 +0900

    Title: 체질량지수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/체질량지수/bmi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    int b = 10000*w / h / h;
+    cout << b << '\n';
+    if (b>=25) cout << "Obesity";
+    return 0;
+}
