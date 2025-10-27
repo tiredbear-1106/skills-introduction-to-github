9c659b3b274c62c0734dc24ffabdcad6a0ec408a
SeokHoon Kim <qttttt@naver.com>
2025-10-27 12:12:05 +0900

    Title: 일 수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251027/일 수 구하기/number-of-days-in-month.cpp
+++ 251027/일 수 구하기/number-of-days-in-month.cpp
@@ -8,7 +8,7 @@ int main() {
     if (n == 2) cout << "28";
     else {
         if ((n<=7 && n%2==1) || (n>=8 && n%2==0)) cout << "31";
-        else cout << "28";
+        else cout << "30";
     }
     return 0;
 }
